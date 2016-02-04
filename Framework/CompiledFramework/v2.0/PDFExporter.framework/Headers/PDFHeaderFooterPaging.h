159cff632fe35f6604b9f3cb3abfb02db22cef25
david-livadaru-3pillarglobal <david-livadaru@3pillarglobal.com>
2016-02-04 09:32:42 +0200

    Preparation for version 2.0.

---
+++ Framework/CompiledFramework/v2.0/PDFExporter.framework/Headers/PDFHeaderFooterPaging.h
@@ -0,0 +1,19 @@
+//
+//  PDFHeaderFooterPaging.h
+//  PDFExporter
+//
+//  Copyright © 2015 3Pillar Global. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+ 
+@protocol PDFHeaderFooterPaging <NSObject>
+
+@optional
+- (void)updatePageNumber:(NSUInteger)pageNumber totalPages:(NSUInteger)totalPages;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Framework/CompiledFramework/v2.0/PDFExporter.framework/Headers/UIView+PDFExporterViewSlicing.h
@@ -0,0 +1,23 @@
+//
+//  UIView+PDFExporterViewSlicing.h
+//  PDFExporter
+//
+//  Copyright © 2016 3Pillar Global. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <PDFExporter/PDFRenderingDelegate.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (PDFExporterViewSlicing)
+
+// content views and subviews that do not fit on one page are sliced
+// a content view is a view just contains other views with no custom drawing
+@property (nonatomic, readonly, getter=shouldSliceSubviews) BOOL sliceSubviews;
+// rendering delegate is the same within a view hierarchy
+@property (nonatomic, weak, nullable) id<PDFRenderingDelegate> renderingDelegate;
+
+@end
+
+NS_ASSUME_NONNULL_END
